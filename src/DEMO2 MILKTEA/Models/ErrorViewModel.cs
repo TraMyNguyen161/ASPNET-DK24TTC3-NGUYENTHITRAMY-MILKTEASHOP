e5bfdeb28f191965cea83d2a4cacc29ff8e95059
Nguyen Thi Tra My <myntt1601002@tvu-onschool.edu.vn>
2025-11-11 22:29:43 +0700

    Tạo Project

---
+++ src/DEMO2 MILKTEA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DEMO2_MILKTEA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
